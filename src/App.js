01bc0c223ade147ed2a2991f95d88e4660a0cb54
hrishi-singh <singh.123@iitj.ac.in>
2023-11-21 02:20:23 +0530

    updated APP.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
+    console.log(data.search);
     setmovies(data.Search);
   };
   useEffect(() => {
